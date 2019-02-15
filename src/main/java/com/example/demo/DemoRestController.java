18af4ac96cf41ee3cd403d62796fc8a5afbff971
Saurabh Mangalam <skmangalam14@gmail.com>
2019-02-15 20:28:21 +0530

    added a hello controller

--- src/main/java/com/example/demo/DemoRestController.java
+++ src/main/java/com/example/demo/DemoRestController.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
+	
+	@RequestMapping("/")
+	public String hello()
+	{
+		return "hello";
+	}
 
 }
